e2d3651aa6827ab67211a5fbdf16a0182fb53256
Dominic Roberts <d.j.roberts42@gmail.com>
2018-12-19 19:13:12 -0600

    Update collectionModel.js

--- cvat/apps/engine/static/engine/js/collectionModel.js
+++ cvat/apps/engine/static/engine/js/collectionModel.js
@@ -192,7 +192,7 @@ class CollectionModel extends Listener {
 
                 }
 
-                skeletons.push([keypoints,skel.frame,skel.outside,0, skel.activity]);
+                skeletons.add([keypoints,skel.frame,skel.outside,0, skel.activity]);
                 for (let attr of skel.attributes) {
                     attributes.push([attr.id, skel.frame, attr.value]);
                 }
